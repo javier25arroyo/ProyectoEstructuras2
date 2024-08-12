17a1cbe828a667f4731651e9fd44bee67db34cad
SebbingtonTaken <128566685+SebbingtonTaken@users.noreply.github.com>
2024-08-11 20:52:48 -0600

    Lectura de archivos y structs

---
+++ ProyectoEstructuras2/Archivos.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+#include "Trabajador.h"
+ 
+using namespace std;
+
+void escribirArchivo(const Trabajador& trabajador, const string& filename) {
+    ofstream archivo(filename);
+
+    if (!archivo) {
+        cerr << "Error al abrir el archivo para escribir." << endl;
+        return;
+    }
+
+    archivo << "Nombre: " << trabajador.nombre << endl;
+    archivo << "Cédula: " << trabajador.cedula << endl;
+    archivo << "Número de Nóminas: " << trabajador.nominas.size() << endl; //anhade la cantidad de nominas al archivo
+
+    for (const auto& nomina : trabajador.nominas) {
+        archivo << "Horas Trabajadas: " << nomina.horasTrabajados << endl;
+        archivo << "Horas Extras: " << nomina.horasExtras << endl;
+        archivo << "Monto a Pagar: " << nomina.montoPagar << endl;
+        archivo << "Deducciones: " << nomina.deducciones << endl;
+    }
+
+    archivo.close();
+    cout << "Datos guardados en el archivo." << endl;
+}
+
+void leerArchivo(Trabajador& trabajador, const string& filename) {
+    ifstream archivo(filename);
+
+    if (!archivo) {
+        cerr << "Error al abrir el archivo para leer." << endl;
+        return;
+    }
+
+    string temp;
+
+    getline(archivo, temp);  //lee la linea y al terminar brinca a la siguiente
+
+    trabajador.nombre = temp.substr(temp.find(": ") + 2);    //busca el formato de : mas dos espacios
+
+    getline(archivo, temp); 
+    trabajador.cedula = temp.substr(temp.find(": ") + 2);
+
+    getline(archivo, temp); 
+    size_t numNominas = stoi(temp.substr(temp.find(": ") + 2));
+    trabajador.nominas.resize(numNominas);
+
+    for (auto& nomina : trabajador.nominas) {
+        getline(archivo, temp); // Skip "Horas Trabajadas: "
+        nomina.horasTrabajados = stoi(temp.substr(temp.find(": ") + 2));
+
+        getline(archivo, temp); // Skip "Horas Extras: "
+        nomina.horasExtras = stoi(temp.substr(temp.find(": ") + 2));
+
+        getline(archivo, temp); // Skip "Monto a Pagar: "
+        nomina.montoPagar = stod(temp.substr(temp.find(": ") + 2));
+
+        getline(archivo, temp); // Skip "Deducciones: "
+        nomina.deducciones = stod(temp.substr(temp.find(": ") + 2));
+    }
+
+    archivo.close();
+    cout << "Datos leídos del archivo." << endl;
+}
--- ProyectoEstructuras2/ProyectoEstructuras2.cpp
+++ ProyectoEstructuras2/ProyectoEstructuras2.cpp
@@ -2,10 +2,66 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
 
+#include "Archivos.h"
+
+#include "Nomina.h"
 int main()
 {
-    std::cout << "Hello World!\n";
+    Trabajador trabajador;
+
+    // ingrese la data del trabajador
+    cout << "Ingrese el nombre del trabajador: ";
+    getline(cin, trabajador.nombre);
+
+    cout << "Ingrese la cédula del trabajador: ";
+    getline(cin, trabajador.cedula);
+
+    size_t numNominas;
+    cout << "Ingrese el número de nóminas: ";
+    cin >> numNominas;
+    trabajador.nominas.resize(numNominas);
+
+    //Ciclo para cada nomina a agregar // lo ideal es agregarla 1 por 1 cuando ya haya interfaz grafica
+    for (size_t i = 0; i < numNominas; ++i) {
+        cout << "\nIngrese los datos para la nómina " << i + 1 << ":\n";
+
+        cout << "Horas trabajadas: ";
+        cin >> trabajador.nominas[i].horasTrabajados;
+
+        cout << "Horas extras: ";
+        cin >> trabajador.nominas[i].horasExtras;
+
+        cout << "Monto a pagar: ";
+        cin >> trabajador.nominas[i].montoPagar;
+
+        cout << "Deducciones: ";
+        cin >> trabajador.nominas[i].deducciones;
+    }
+
+
+    string filename = trabajador.nombre+".txt";
+    //escribir el archivo
+    escribirArchivo(trabajador, filename);
+
+    //Leer archivo
+    Trabajador trabajadorLeido;
+    leerArchivo(trabajadorLeido, filename);
+
+    //Imprimir Datos
+    cout << "Nombre: " << trabajadorLeido.nombre << endl;
+    cout << "Cédula: " << trabajadorLeido.cedula << endl;
+    for (const auto& nomina : trabajadorLeido.nominas) {
+        cout <<"------Nomina------" << endl;
+        cout << "Horas Trabajadas: " << nomina.horasTrabajados << endl;
+        cout << "Horas Extras: " << nomina.horasExtras << endl;
+        cout << "Monto a Pagar: " << nomina.montoPagar << endl;
+        cout << "Deducciones: " << nomina.deducciones << endl;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
